42b34821dc525b680867117549b081709196bd4d
so-c <so_c321@hotmail.co.jp>
2021-09-25 14:30:11 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # soc-toolbox
+
 主に動画編集に使うちょっとしたツール類
+
+## voicevox-caller
+
+PowerShellからVOICEVOXの音声合成エンジンを呼ぶためのスクリプトです。
+
+### Recotte Studioの「ユーザー定義音声連携」での使い方
+
+#### インストール手順
+
+1. `voicevox-caler.ps1`と`VOICEVOX(PowerShell 5.x).rvls`をダウンロードして好きなフォルダに保存する
+1. Recotte Studioの [環境設定 > ユーザー定義音声連携の設定] を開く
+1. インポートボタンを押して`VOICEVOX(PowerShell 5.x).rvls`を読み込む
+1. [引数] で `-File ""` のダブルクォーテーションの中に`voicevox-caler.ps1`のフルパスを書き足す
+   * `-ExecutionPolicy RemoteSigned -File "C:\Users\YourName\Documents\voicevox-caller\voicevox-caller.ps1" -text "%c" -speaker "%s" -output "%o"`
+1. [適用]ボタンを押す
+
+#### 準備
+
+1. 話者レイヤーを追加してプロパティーを開く
+1. [話者名] (≠名前) を「四国めたん」か「ずんだもん」で始まるようにする
+1. [音声連携] でインストールした"VOICEVOX(PoerShell 5.x)" を選ぶ
+1. [OK] ボタンを押す
+
+#### 使い方
+
+VOICEVOXを起動した状態で[音声の同期] をしてください
+
+#### ヒント
+
+* 音声が作成されない場合は`voicevox-caler.ps1`と同じフォルダの`last_error.txt`を確認してみてください。最後に発生したエラーが出力されています
+* 調整パラメータを [ユーザ定義音声連携] の引数として渡せます。利用できるパラメータはVOICEVOXのドキュメント (http://127.0.0.1:50021/docs) を参照してください
+* このスクリプトはWindowsに最初からインストールされてるWindows PowerShell ISEで変更・動作確認できます。好みに合わせてカスタマイズして使ってください
---
+++ voicevox-caller/voicevox-caller.ps1
@@ -0,0 +1,77 @@
+﻿Param($text, $speaker, $output,
+    # パラメータの意味は VOICEVOXを起動して
+    # http://127.0.0.1:50021/docs にアクセスして確認
+    $speedScale         = 1.0,
+    $pitchScale         = 0.0,
+    $intonationScale    = 1.0,
+    $volumeScale        = 1.0,
+    $prePhonemeLength   = 0.1,
+    $postPhonemeLength  = 0.1,
+    $outputSamplingRate = 24000,
+    $outputStereo       = $false
+)
+
+$logFile = "$PSScriptRoot\last_error.txt"
+function Write-Log($msg) {
+    $date = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+    Write-Error "$date`t$msg" 2>&1 > $logFile
+}
+
+# 必須パラメータチェック
+if (-not $text) {
+    Write-Log("１文字以上のテキストを入力してください")
+    return
+}
+Add-Type -AssemblyName System.Web
+$encoded_text = [System.Web.HttpUtility]::UrlEncode($text)
+
+if ($speaker -Like "四国めたん*") {
+    $speaker_param = 0
+} elseif ($speaker -Like "ずんだもん*") {
+    $speaker_param = 1
+} else {
+    Write-Log("話者名(≠レイヤー名）を「四国めたん」か「ずんだもん」で始めてください")
+    return
+}
+
+if (-not $output) {
+    Write-Log("出力先ファイル名を指定してください")
+    return
+}
+
+[System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+# VOICEVOX 音声合成クエリ作成
+try {
+    $query = Invoke-RestMethod `
+        -Method POST `
+        -Uri "http://127.0.0.1:50021/audio_query?text=$encoded_text&speaker=$speaker_param"
+} catch {
+    Write-Log($PSItem)
+    return
+}
+
+# 調整パラメータをユーザ指定に置き換え
+$query.speedScale = $speedScale
+$query.pitchScale = $pitchScale
+$query.intonationScale = $intonationScale
+$query.volumeScale        = $volumeScale
+$query.prePhonemeLength   = $prePhonemeLength
+$query.postPhonemeLength  = $postPhonemeLength
+$query.outputSamplingRate = $outputSamplingRate
+$query.outputStereo       = $outputStereo
+
+$configured_query = ConvertTo-Json $query -Depth 8
+
+# VOICEVOX 音声合成API実行
+try  {
+    Invoke-RestMethod `
+        -Method POST `
+        -Uri http://127.0.0.1:50021/synthesis?speaker=$speaker_param `
+        -ContentType "application/json" `
+        -Body $configured_query `
+        -OutFile $output
+} catch {
+    Write-Log($PSItem)
+    return
+}
