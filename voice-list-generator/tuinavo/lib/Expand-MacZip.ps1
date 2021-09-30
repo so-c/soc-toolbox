a39b42f0a3000d93b795bcc3282fbef167778a4c
so-c <so_c321@hotmail.co.jp>
2021-10-01 08:14:46 +0900

    第43回以降のフォルダ構造に対応

--- voice-list-generator/tuinavo/lib/Expand-MacZip.ps1
+++ voice-list-generator/tuinavo/lib/Expand-MacZip.ps1
@@ -1,4 +1,16 @@
 ﻿Add-Type -AssemblyName System.IO.Compression.FileSystem
+function NeedsSubDirectory {
+    param (
+        [string]$FullName
+    )
+
+    if ($FullName.EndsWith('_free/wav/') -or $FullName.EndsWith('_for500/wav/') -or $FullName.EndsWith('_for1000/wav/')) {
+        return $false
+    }
+
+    return $true
+}
+
 function Expand-MacZip {
     [CmdletBinding()]
     param (
@@ -6,31 +18,37 @@ function Expand-MacZip {
     )
     $archive = [System.IO.Compression.ZipFile]::OpenRead($zipPath)
     foreach ($e in $archive.Entries) {
+
+        if ($e.FullName.Contains("/mp3")) {
+            continue
+        }
+
         if ($e.FullName.EndsWith('/')) {
-            $dirName = "wav\" + $e.FullName.TrimEnd('/') + "\"
+            if (-not (NeedsSubDirectory($e.FullName))) {
+                continue
+            }
+            $dirName = 'wav\' + ($e.FullName -replace '/', '\')
             New-Item -Path $dirName -ItemType Directory > $null
             continue
         }
-        $entryName = $e.FullName.Normalize() -replace '/', '\'
+
         # Windowsのファイルシステムでは使えない文字の置換
+        $entryName = $e.FullName.Normalize() -replace '/', '\'
         $entryName = $entryName -replace ':', '_'
-        $entryName = $entryName -replace "!\?", "！？" # !？[半角・全角]は表示が乱れるのでペアで置換
+        $entryName = $entryName -replace '!\?', '！？' # !？[半角・全角]は表示が乱れるのでペアで置換
         # 濁音、半濁音の置換
         # TODO：
         # ad-hocなファイル名誤り修正
         $entryName = $entryName -replace '116 ウチが主催しとる妖怪系オンリー', 'ウチが主催しとる妖怪系オンリー'
-        $dest = [System.IO.Path]::Combine((Resolve-Path ".\wav"), $entryName)
+        $entryName = $entryName -replace '\\wav', '' # 第43回以降のフォルダ構造を第42回以前に合わせる
+        $dest = [System.IO.Path]::Combine((Resolve-Path '.\wav'), $entryName)
         try {
             [System.IO.Compression.ZipFileExtensions]::ExtractToFile($e, $dest, $true)
         }
         catch [System.NotSupportedException] {
+            Write-Host "$e to $dest"
             Write-Error ”パス：$dest”
-            Write-Error $PSItem
-        }
-        catch {
-            Set-Content "log.txt" -Value  $e -Encoding UTF8 
-            Set-Content "log2.txt" -Value  ”パス：$dest” -Encoding UTF8
-            Write-Error $PSItem
+            throw $PSItem
         }
     }
 }
