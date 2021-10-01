3188f3a87ce35c5d16056342b72a46c7880743fc
so-c <so_c321@hotmail.co.jp>
2021-10-01 18:44:13 +0900

    fix #13 add Pitagoe class

---
+++ voice-list-generator/tuinavo/lib/Pitagoe.ps1
@@ -0,0 +1,21 @@
+﻿class PitagoeRecord {
+    $FilePath
+    [string]$DisplayName
+    [string]$Serifu
+    [string]$Yomigana
+    [string]$Category
+
+    PitagoeRecord(
+        $f,
+        [string]$dn,
+        [string]$s,
+        [string]$y,
+        [string]$c
+    ) {
+        $this.FilePath = $f
+        $this.DisplayName = $dn
+        $this.Serifu = $s
+        $this.Yomigana = $y
+        $this.Category = $c
+    }
+}
