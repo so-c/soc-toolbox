c5e186437a946cfd7682fa6c083dbcebeafb1fd4
so-c <so_c321@hotmail.co.jp>
2021-10-09 08:58:06 +0900

    #24 サンプルボイスとexVOICE両対応準備

--- voice-list-generator/tuinavo/lib/PitagoeRecord.ps1
+++ voice-list-generator/tuinavo/lib/PitagoeRecord.ps1
@@ -28,7 +28,7 @@
         '&#x2469;'  = '⑩'
         '&#x301;'   = '́'
         '&#x167;'   = 'ŧ'
-        '&#x2039;_'  = '‹" '
+        '&#x2039;_' = '‹" '
         '_'         = '/'    # ファイル名に使えないため置換された文字を戻す
     }
 
@@ -39,21 +39,26 @@
         '2/ついなちゃん啓蒙動画/うがい' = 'うがいの仕方は、３回、口やのどをゆすぐのがポイントや！ 最初は、まず水を口に含み、少し強めに口の中をクチュクチュゆすいで、ぺッて吐き出すで〜。続いて水を口に含み、上を向いて15秒ほどのどの奥でガラガラうがいをするで。１回１５秒、これを２回繰り返すんや！ クチュクチュぺッ、ガラガラぺッ、ガラガラぺッ☆　これ、覚えといてな〜♫'
         '3/ついなちゃん啓蒙動画/手洗い' = '手を洗う時は、蛇口から流れる水で手を濡らして、まず石鹸で丁寧に手のひらをこすり洗い。手の甲、指先、爪の間と順番に、念入りにこすること！　指と指の間もしっかり洗うてな☆　親指や手のひらをねじるように洗うのもポイント！　そうそう、手首も忘れずに洗うてな〜！　──そして手洗いの時間は１分くらい。洗った後は、 濡れた手はペーパータオルか清潔なタオルで拭いてね♡'
         '4/ついなちゃん啓蒙動画/マスク' = '疫病が流行しとる時は、忘れずにしっかりマスクするのがマナーやで〜。マスクを着けるときは、鼻と口の両方を覆って、隙間がないようにしっかりと装着してな！　マスクない時は、ティッシュやハンカチで口・鼻を覆ってね！　とっさの時は他の人のいない方を向いて、お洋服の袖で口・鼻をカバーしてな！　これぞ、必殺技「肘ブロック」やで☆'
-        '(/ /////////` )' = '(́ _________` )'
-        '( ｰ`дｰ/)'        = '( ｰ`дｰ´)'
-        'え/ち'            = 'えっち'
-        '(/ /)'           = '(> <)'
-        'ω/-)'            = 'ω´-)'
-        '( ´∀｀)人(´∀｀ )' = '( ´∀｀)人(´∀｀ )ﾅｶｰﾏ'
+        '(/ /////////` )'  = '(́ _________` )'
+        '( ｰ`дｰ/)'         = '( ｰ`дｰ´)'
+        'え/ち'              = 'えっち'
+        '(/ /)'            = '(> <)'
+        'ω/-)'             = 'ω´-)'
+        '( ´∀｀)人(´∀｀ )'    = '( ´∀｀)人(´∀｀ )ﾅｶｰﾏ'
     }
 
     static $categories
     static $displayNumbers
     static $yomiganaDic
+    static $displayNumbersExvo
+    static $yomiganaDicExvo
     static PitagoeRecord() {
         [PitagoeRecord]::categories = (Get-Content $PSScriptRoot\category.json | ConvertFrom-Json)
         [PitagoeRecord]::displayNumbers = (Get-Content $PSScriptRoot\displayNumbers.json | ConvertFrom-Json)
         [PitagoeRecord]::yomiganaDic = (Get-Content $PSScriptRoot\yomigana.json | ConvertFrom-Json)
+
+        [PitagoeRecord]::displayNumbersExvo = (Get-Content $PSScriptRoot\displayNumbersExvo.json | ConvertFrom-Json)
+        [PitagoeRecord]::yomiganaDicExvo = (Get-Content $PSScriptRoot\yomiganaExvo.json | ConvertFrom-Json)
     }
 
     [string] GetSerif([string]$baseName) {
@@ -95,6 +100,11 @@
         return $fsi.Directory.Name
     }
 
+    [string] GetDisplayNumber($fsi) {
+        $relativePath = ((Split-Path $fsi.DirectoryName -Leaf) + "\" + $fsi.Name)
+        return [PitagoeRecord]::displayNumbers.$relativePath + " " + $this.GetSerif($fsi.BaseName)
+    }
+
     PitagoeRecord(
         $f,
         [string]$bn,
@@ -113,7 +123,7 @@
     ) {
         $relativePath = ((Split-Path $fsi.DirectoryName -Leaf) + "\" + $fsi.Name)
         $this.FilePath = $relativePath
-        $this.DisplayName = [PitagoeRecord]::displayNumbers.$relativePath + " " + $this.GetSerif($fsi.BaseName)
+        $this.DisplayName = $this.GetDisplayNumber($fsi)
         $this.Serifu = $this.GetSerif($fsi.BaseName)
         $this.Yomigana = [PitagoeRecord]::yomiganaDic.$relativePath
         $this.Category = $this.GetCategory($fsi)
