16798fc4916aa64ffc8fc914c35199105b812a80
so-c <so_c321@hotmail.co.jp>
2022-01-01 11:22:37 +0900

    #64 雨晴はうさん対応

--- voicevox-caller/src/functions/Find-SpeakerParam.ps1
+++ voicevox-caller/src/functions/Find-SpeakerParam.ps1
@@ -5,19 +5,19 @@ $COEIROINK_URL = "http://127.0.0.1:50031"
 
 function Find-SpeakerParam($speaker) {
     if ($speaker -Like "四国めたん*") {
-        return　@{
+        return @{
             baseUrl = $VOICEVOX_URL
             id      = (0 + (Get-StyleIndex($speaker)))
         }
     }
     elseif ($speaker -Like "ずんだもん*") {
-        return　@{
+        return @{
             baseUrl = $VOICEVOX_URL
             id      = (1 + (Get-StyleIndex($speaker)))
         }
     }
     elseif ($speaker -Like "春日部つむぎ*") {
-        return　@{
+        return @{
             baseUrl = $VOICEVOX_URL
             id      = 8
         }
@@ -26,6 +26,12 @@ function Find-SpeakerParam($speaker) {
         return @{
             baseUrl = $VOICEVOX_URL
             id      = 9
+        }
+    }
+    elseif ($speaker -Like "雨晴はう*") {
+        return @{
+            baseUrl = $VOICEVOX_URL
+            id      = 10
         }
     }
     elseif ($speaker -Like "つくよみちゃん*") {
@@ -41,7 +47,7 @@ function Find-SpeakerParam($speaker) {
         }
     }
     else {
-        Write-Log("VOICEVOXの場合、話者名(≠レイヤー名）を「四国めたん」、「ずんだもん」、「春日部つむぎ」、「波音リツ」で始めてください。COEIROINKの場合、「つくよみちゃん」または「MANA」（大文字小文字全角半角問わず）で始めてください")
+        Write-Log("VOICEVOXの場合、話者名(≠レイヤー名）を「四国めたん」、「ずんだもん」、「春日部つむぎ」、「雨晴はう」、「波音リツ」で始めてください。COEIROINKの場合、「つくよみちゃん」または「MANA」（大文字小文字全角半角問わず）で始めてください")
         return
     }
 }
