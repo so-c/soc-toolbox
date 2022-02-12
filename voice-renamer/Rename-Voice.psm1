6ecd269928ce803d5b4ac5664fcc3b5b878cfad7
so-c <so_c321@hotmail.co.jp>
2022-02-12 10:39:18 +0900

    ボイロの出力wav, txtを同期して変更するPS1モジュールを追加

---
+++ voice-renamer/Rename-Voice.psm1
@@ -0,0 +1,28 @@
+﻿function Rename-Voice {
+
+    <#
+    .SYNOPSIS
+        VOICEROID2の出力するwavとtxtのファイル名にtxtファイル1行目冒頭（最長20文字）を追加する
+    .PARAMETER folder
+        wavとtxtを出力したフォルダ
+    #>
+
+    [CmdletBinding(SupportsShouldProcess)]
+    Param(
+        [Parameter(Mandatory,
+            ValueFromPipeline)]
+        $folder
+    )
+
+    PROCESS {
+        Get-ChildItem "$folder\*.wav" | ForEach-Object {
+            $textFilePath = "$folder\$($_.BaseName).txt"
+            $firstLine = Get-Content $textFilePath -Head 1
+            $suffix = $firstLine.Substring(0, [Math]::Min(20, $firstLine.Length))
+
+            Rename-Item -Path $_ -NewName ('{0}_{1}.wav' -f $_.BaseName, $suffix) -WhatIf:$WhatIfPreference
+            Rename-Item -Path $textFilePath -NewName ('{0}_{1}.txt' -f $_.BaseName, $suffix) -WhatIf:$WhatIfPreference
+        }
+    }
+
+}
