70576887460445f6ae5a94266bb096ade60ec0a6
so-c <so_c321@hotmail.co.jp>
2022-04-17 08:55:24 +0900

    #104 リリーススクリプトの修正

--- Prepare-Release.ps1
+++ Prepare-Release.ps1
@@ -4,35 +4,38 @@
     $version
 )
 
-$tuinavo = "tuinavo-list-generator-$version"
 $voivoxCaller = "voicecox-caller-$version"
 $aivosUserDef = "aivos-userdef-$version"
-
-$PSScriptRoot
 
 if (-not (Test-Path $PSScriptRoot\release)) {
     New-Item $PSScriptRoot\release -ItemType Directory > $null
 }
 Remove-Item $PSScriptRoot\release\* -Recurse -Force
 
-# ついなボリストジェネレータ
-New-Item "$PSScriptRoot\release\$tuinavo" -ItemType Directory > $null
+function Zip-Tuinavo($tuinavo) {
+    # ついなボリストジェネレータ
+    New-Item "$PSScriptRoot\release\$tuinavo" -ItemType Directory > $null
 
-Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\*.* $PSScriptRoot\release\$tuinavo\
-Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\lib $PSScriptRoot\release\$tuinavo\ -Recurse
-Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\resource $PSScriptRoot\release\$tuinavo\ -Recurse
-Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\omake $PSScriptRoot\release\$tuinavo\ -Recurse
-New-Item -Path $PSScriptRoot\release\$tuinavo\ex, `
-               $PSScriptRoot\release\$tuinavo\wav, `
-               $PSScriptRoot\release\$tuinavo\zip `
-         -ItemType Directory >$null
-Copy-Item -Path $PSScriptRoot\README.md, $PSScriptRoot\LICENSE -Destination $PSScriptRoot\release\$tuinavo
-Compress-Archive -Path $PSScriptRoot\release\$tuinavo -DestinationPath $PSScriptRoot\release\$tuinavo.zip
+    Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\*.* $PSScriptRoot\release\$tuinavo\
+    Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\lib $PSScriptRoot\release\$tuinavo\ -Recurse
+    Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\resource $PSScriptRoot\release\$tuinavo\ -Recurse
+    Copy-Item $PSScriptRoot\voice-list-generator\tuinavo\omake $PSScriptRoot\release\$tuinavo\ -Recurse
+    New-Item -Path $PSScriptRoot\release\$tuinavo\ex, `
+        $PSScriptRoot\release\$tuinavo\wav, `
+        $PSScriptRoot\release\$tuinavo\zip, `
+        $PSScriptRoot\release\$tuinavo\Saki_wav, `
+        $PSScriptRoot\release\$tuinavo\Saki_zip `
+        -ItemType Directory >$null
+    Copy-Item -Path $PSScriptRoot\README.md, $PSScriptRoot\LICENSE -Destination $PSScriptRoot\release\$tuinavo
+    Compress-Archive -Path $PSScriptRoot\release\$tuinavo -DestinationPath $PSScriptRoot\release\$tuinavo.zip
+}
+
+Zip-Tuinavo("tuinavo-list-generator-$version")
 
 # voicevox-caller
 New-Item "$PSScriptRoot\release\$voivoxCaller" -ItemType Directory > $null
 
-if(Test-Path $PSScriptRoot\voicevox-caller\src\last_error.txt) {
+if (Test-Path $PSScriptRoot\voicevox-caller\src\last_error.txt) {
     Remove-Item $PSScriptRoot\voicevox-caller\src\last_error.txt
 }
 Copy-Item $PSScriptRoot\voicevox-caller\src\*.* $PSScriptRoot\release\$voivoxCaller\
