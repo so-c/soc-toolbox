a26de8032586f8d27bd40437be5b99e896377ac9
so-c <so_c321@hotmail.co.jp>
2022-05-01 16:19:54 +0900

    常に先頭行にInsertするように変更。リスト長を取得するAPIがないため末尾をSelectするのに時間がかかるため。

--- aivos-caller/aivos-caller.ps1
+++ aivos-caller/aivos-caller.ps1
@@ -4,50 +4,59 @@
     $output
 )
 
-$settings = Get-Content $PSScriptRoot\settings.json | ConvertFrom-Json
-Add-Type -Path $settings.path
-
-$TTS = [AI.Talk.Editor.Api.TtsControl]::new()
-
-if ($TTS.GetAvailableHostNames().Length -eq 0) {
-    Write-Error "A.I.VOICE Editorが未インストールです" -ErrorAction Stop
+Function Get-TTS($Path) {
+    $Settings = Get-Content $PAth\settings.json | ConvertFrom-Json
+    Add-Type -Path $settings.path
+    
+    return [AI.Talk.Editor.Api.TtsControl]::new()
 }
 
-try {
-    if (-not $TTS.IsInitialized) {
-        $TTS.Initialize(($TTS.GetAvailableHostNames()[0])) # 冪等なら過剰防御
+Function Connect-TTS($TTS) {
+    if ($TTS.GetAvailableHostNames().Length -eq 0) {
+        Write-Error "A.I.VOICE Editorが未インストールです" -ErrorAction Stop
     }
-}
-catch {
-    Write-Error "A.I.Editor APIの初期化に失敗しました。" -ErrorAction Stop # 発生条件がよくわからない
-}
-
-if ($TTS.Status -eq [AI.Talk.Editor.Api.HostStatus]::NotRunning) {
-    Write-Error "起動中のA.I.VOICE Editorが見つかりません。起動後に再実行してください。" -ErrorAction Stop
-}
-
-try {
-    $TTS.Connect()
-} catch {
-    Write-Error "A.I.VOICE Editorと接続できませんでした。" -ErrorAction Stop
-}
-
-try {
-    $TTS.TextEditMode = [AI.Talk.Editor.Api.TextEditMode]::List
-
-    $i = 0
+    
     try {
-        $TTS.AddListItem($speaker, $text)
-        while ($True) {
-          $TTS.SetListSelectionIndex($i)
-          $i++
+        if (-not $TTS.IsInitialized) {
+            $TTS.Initialize(($TTS.GetAvailableHostNames()[0])) # 冪等なら過剰防御
         }
     }
+    catch {
+        Write-Error "A.I.Editor APIの初期化に失敗しました。" -ErrorAction Stop # 発生条件がよくわからない
+    }
+    
+    if ($TTS.Status -eq [AI.Talk.Editor.Api.HostStatus]::NotRunning) {
+        Write-Error "起動中のA.I.VOICE Editorが見つかりません。起動後に再実行してください。" -ErrorAction Stop
+    }
+    
+    try {
+        $TTS.Connect()
+    }
+    catch {
+        Write-Error "A.I.VOICE Editorと接続できませんでした。" -ErrorAction Stop
+    }
+}
+
+Function Add-ItemOnTop($TTS, $speaker, $text) {
+    $TTS.TextEditMode = [AI.Talk.Editor.Api.TextEditMode]::List
+
+    try {
+        $TTS.SetListSelectionIndex(0)
+    }
     catch [InvalidOperationException] {
-        # Listの行数を取得するAPIがないので
-        # エラーを握りつぶす
+        $TTS.AddListItem($speaker, $text)
+        return
     }
 
+    $TTS.SetListSelectionIndex(0)
+    $TTS.InsertListItem($speaker, $text)
+}
+
+$TTS = Get-TTS $PSScriptRoot
+Connect-TTS $TTS
+
+try {
+    Add-ItemOnTop -TTS $TTS -speaker $speaker -text $text
     $TTS.SaveAudioToFile($output)
 }
 catch {
