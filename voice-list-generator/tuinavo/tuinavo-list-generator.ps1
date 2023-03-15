db9eb3d1b9b482d4d3e7e7d345bf76da40b7b90b
so-c <so_c321@hotmail.co.jp>
2023-03-15 20:01:53 +0900

    #142 サンプルボイスとexVOICEとでスクリプトを分離

--- voice-list-generator/tuinavo/tuinavo-list-generator.ps1
+++ voice-list-generator/tuinavo/tuinavo-list-generator.ps1
@@ -1,44 +1,26 @@
 ﻿Param(
-    [Parameter(Mandatory)]
-    [ValidateSet("Sample", "SampleCF", "Ex")]
-    [String]
-    $Voice,
     [switch]$NoExpand
 )
+
 # usage: 下記のように配置して実行
+#        zipファイルをwavフォルダに展開済みの場合、
+#        -noExpandオプションをつけてください
 # .\
 #   + tuinavo-list-generator.ps1
 #   + zip\
-#       + abcd1234_Sample_voice_050.zip
+#     + abcd1234_Sample_voice_050.zip
+
 $workDir = $PSScriptRoot
 . "$workDir\lib\Expand-MacZip.ps1"
 . "$workDir\lib\PitagoeRecord.ps1"
-. "$workDir\lib\PitagoeRecordExvo.ps1"
-. "$workDir\lib\Rename-SVCFFolders.ps1"
-
-$destDir
-$csvFileName
-
-switch -Wildcard ($Voice) {
-    "Sample*" {
-        $destDir = "$workDir\wav"
-        $csvFileName = "ついなちゃん セリフ集.csv"
-    }
-    "Ex" {
-        $destDir = "$workDir\ex"
-        $csvFileName = "ついなちゃん exVOICE.csv"
-    }
-    Default {
-        Write-Error "到達不能コード"
-    }
-}
+$destDir = "$workDir\wav"
+$csvFileName = "ついなちゃん セリフ集.csv"
 
 function Expand-SampeZips {
-    Write-Host "zipフォルダ内のzipファイルを展開します"
     if (Test-Path $destDir) {
         Get-ChildItem $destDir\* -Recurse |
         ForEach-Object {
-            if ($_.FullName.Contains("第5回") -or $_.FullName.Contains("第8回") -or $_.FullName.Contains("第21回")) {
+            if ($_.FullName -match '第(5|8|21)回') {
                 # zipエラーで展開できないので同期しない
             }
             elseif ($_.Name -eq "thumbnail.png") {
@@ -56,27 +38,16 @@ function Expand-SampeZips {
     Get-ChildItem $workDir\zip\*.zip | ForEach-Object {
         Expand-MacZip($_.FullName)
     }
-    
-    Write-Host "展開を完了しました"
-    Write-Host "第5, 8, 21回は展開できないので、まだなら手動で展開したあと、実行し直してください"
-    Write-Host "-NoExpandオプションをつけると再展開しないので気持ち速くなります"    
 }
 
-######## ここからmain相当 ########
-
-if ($Voice -like "Sample*" -and -not $NoExpand) {
+if (-not $NoExpand) {
+    Write-Host "zipフォルダ内のzipファイルを展開します"
     Expand-SampeZips
+    Write-Host "展開を完了しました"
+    Write-Host "第5, 8, 21回は展開できないので、まだなら手動で展開したあと、-NoExpandオプションをつけて再実行してください"
 }
 
-if ($Voice -eq "SampleCF" -and -not $NoExpand) {
-    Rename-SVCFFolders
-}
-
-$pitagoes = switch -Wildcard ($Voice) {
-    "Sample*" { [PitagoeRecord]::newPitagoeList("$destDir\") }
-    "Ex" { [PitagoeRecordExvo]::newPitagoeList("$destDir\") }
-    Default {}
-}
+$pitagoes = [PitagoeRecord]::newPitagoeList("$destDir")
 
 # ぴた声アプリが表示名ではなくCSVでの登場順に表示するのでソートを挟む
 # XXX：下記対応が不完全
@@ -84,15 +55,12 @@ $pitagoes = switch -Wildcard ($Voice) {
 #   → ※マニュアルに規定なし。動いているように見える
 # ・文字列の引用符はエスケープされて "" になる
 #   → "" のまま表示されてしまうのでデータパッチ
-# ・1行目に空行
-#   → スキップする
 $UTF8noBOM = [System.Text.UTF8Encoding]::new($false, $true)
 $CsvContents = ($pitagoes | Sort-Object -Property DisplayName | ConvertTo-Csv -NoTypeInformation |
     ForEach-Object { $_.Replace('‹" ', "‹''") } |
     Select-Object -Skip 1)
 [System.IO.File]::WriteAllLines("$destDir\$csvFileName", $CsvContents, $UTF8noBOM)
-
 Copy-Item $workDir\resource\character.ini $destDir -Force
-
-Write-Host "${Split-Path $destDir -Leaf}フォルダに「$csvFileName」を作成しました"
-Write-Host "${Split-Path $destDir -Leaf}フォルダを好きな位置・名前に変更して、ぴた声アプリに追加してください"
+$folderName = (Split-Path $destDir -Leaf)
+Write-Host "${folderName}フォルダに「$csvFileName」を作成しました"
+Write-Host "${folderName}フォルダを好きな位置・名前に変更して、ぴた声アプリに追加してください"
---
+++ voice-list-generator/tuinavo/tuinavo-list-generator_ex.ps1
@@ -0,0 +1,20 @@
+﻿$destDir = "$workDir\ex"
+$csvFileName = "ついなちゃん exVOICE.csv"
+
+$pitagoes = [PitagoeRecordExvo]::newPitagoeList("$destDir\")
+
+# ぴた声アプリが表示名ではなくCSVでの登場順に表示するのでソートを挟む
+# XXX：下記対応が不完全
+# ・必ず引用符がつく
+#   → ※マニュアルに規定なし。動いているように見える
+# ・文字列の引用符はエスケープされて "" になる
+#   → "" のまま表示されてしまうのでデータパッチ
+$UTF8noBOM = [System.Text.UTF8Encoding]::new($false, $true)
+$CsvContents = ($pitagoes | Sort-Object -Property DisplayName | ConvertTo-Csv -NoTypeInformation |
+    ForEach-Object { $_.Replace('‹" ', "‹''") } |
+    Select-Object -Skip 1)
+[System.IO.File]::WriteAllLines("$destDir\$csvFileName", $CsvContents, $UTF8noBOM)
+Copy-Item $workDir\resource\character.ini $destDir -Force
+$folderName = (Split-Path $destDir -Leaf)
+Write-Host "${folderName}フォルダに「$csvFileName」を作成しました"
+Write-Host "${folderName}フォルダを好きな位置・名前に変更して、ぴた声アプリに追加してください"
