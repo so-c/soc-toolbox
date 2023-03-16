16752c3b0217843bfc781d39d83a074aaa59e09d
so-c <so_c321@hotmail.co.jp>
2023-03-16 17:41:55 +0900

    #196 テキストファイルの文字コード変換処理を追加!

--- voice-renamer/Rename-Voice.psm1
+++ voice-renamer/Rename-Voice.psm1
@@ -2,7 +2,7 @@
 
     <#
     .SYNOPSIS
-        VOICEROID2の出力するwavとtxtのファイル名にtxtファイル1行目冒頭（最長20文字）を追加する
+        VOICEROID2の出力するwavとtxtのファイル名にtxtファイル1行目冒頭（最長20文字）を追加し、文字コードをUTF8にする
     .PARAMETER folder
         wavとtxtを出力したフォルダ
     #>
@@ -21,9 +21,12 @@
             $firstLine = Get-Content $textFilePath -Head 1 -Encoding $Encoding
             $suffix = $firstLine.Substring(0, [Math]::Min(20, $firstLine.Length))
 
+            $content = Get-Content $textFilePath -Encoding $Encoding
+            Set-Content $textFilePath $content -Encoding UTF8
+
             Rename-Item -Path $_ -NewName ('{0}_{1}.wav' -f $_.BaseName, $suffix) -WhatIf:$WhatIfPreference
-            Rename-Item -Path $textFilePath -NewName ('{0}_{1}.txt' -f $_.BaseName, $suffix) -WhatIf:$WhatIfPreference
+            $newTextFileName = ('{0}_{1}.txt' -f $_.BaseName, $suffix)
+            Rename-Item -Path $textFilePath -NewName $newTextFileName -WhatIf:$WhatIfPreference
         }
     }
-
 }
