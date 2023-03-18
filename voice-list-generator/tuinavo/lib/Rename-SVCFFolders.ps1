82c1615d5f3c401e109d60e49a8c640b77572986
so-c <so_c321@hotmail.co.jp>
2023-03-18 21:13:36 +0900

    SynthV CFリターンのzipから解凍する場合の判定誤りと、Fantiaのzipを解凍するときにも起こる副作用を修正

--- voice-list-generator/tuinavo/lib/Rename-SVCFFolders.ps1
+++ voice-list-generator/tuinavo/lib/Rename-SVCFFolders.ps1
@@ -1,13 +1,11 @@
 ﻿function Move-ChildFolders($zipNumber, $planName, $destDir) {
     foreach ($zn in $zipNumber) {
-        if (Test-Path ("$destDir\Sample_voice_${zn}_wav\Sample_voice_${zn}_${pn}")) {
-            Write-Host "SynthV CFリターンの第${_}回のフォルダ構成をFantiaで配布されているフォルダ構成に合わせます"
-            foreach ($pn in $planName.Keys ) {
+        foreach ($pn in $planName.Keys ) {
+            if (Test-Path ("$destDir\Sample_voice_${zn}_wav\Sample_voice_${zn}_${pn}")) {
                 Move-Item -Pass "$destDir\Sample_voice_${zn}_wav\Sample_voice_${zn}_${pn}" `
-                    -Destination "$destDir\Sample_voice_${zn}_$($planName[$pn])"
+                    -Destination "$destDir\Sample_voice_${zn}_$($planName[$pn])" >$null
+                Remove-Item -Path "$destDir\Sample_voice_${zn}_wav"
             } 
-            Remove-Item -Path "$destDir\Sample_voice_${zn}_wav"
-            Write-Host "第${zn}回のフォルダをリネームしました"
         }
     }
 }
@@ -19,7 +17,7 @@ function Rename-SVCFFolders($destDir) {
         'for1000円プラン' = 'for1000';
         'free'        = 'free'
     }
-    Move-ChildFolders($zipNumber1, $planName1, $destDir)
+    Move-ChildFolders $zipNumber1 $planName1 $destDir
 
     $zipNumber2 = @('047', '049', '050', '051', '052')
     $planName2 = @{
@@ -27,7 +25,7 @@ function Rename-SVCFFolders($destDir) {
         'for1000' = 'for1000';
         'free'    = 'free'
     }
-    Move-ChildFolders($zipNumber2, $planName2, $destDir)
+    Move-ChildFolders $zipNumber2 $planName2 $destDir
 
     # XXX: 空フォルダ"wav_SynthesizerV CF向け限定ボイス（5000円プラン以上向け）"が作成されるので削除する
     $svcf5000Folder = "$destDir\wav_SynthesizerV CF向け限定ボイス（5000円プラン以上向け）"
