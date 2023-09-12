0b255f64e85125530f10edf73c3fe7a9faaa9134
so-c <so_c321@hotmail.co.jp>
2023-09-13 07:24:00 +0900

    #244 おふとんP+を指定してもおふとんPと解釈されるバグ、きざささやきを指定してもきざと解釈されるバグの修正

--- coeica/src/functions/Speakers.ps1
+++ coeica/src/functions/Speakers.ps1
@@ -2,6 +2,19 @@
 
 . $PSScriptRoot\CoeiroinkEngine.ps1
 
+function Find-Speaker($speakerName) {
+  $speakers = Get-NormalizedSpeakers
+
+  $speaker = Find-SpeakerByName $speakerName $speakers
+  $uuid = $speaker.speakerUuid
+  $styleId = Find-StyleByName $speakerName $speaker.styles
+
+  return @{
+    uuid    = $uuid
+    styleId = $styleId
+  }
+}
+
 function Get-NormalizedSpeakers() {
   $speakersRawResponse = Invoke-Speakers
   $minifiedSpeakers = $speakersRawResponse -replace ',(\"base64Icon\":|\"base64Portrait\":)(\".+?\"|null)', ''
@@ -9,24 +22,39 @@ function Get-NormalizedSpeakers() {
   return $speakers
 }
 
-function Find-Speaker($speakerName) {
-  $speakers = Get-NormalizedSpeakers
 
-  $uuid = ($speakers | Where-Object { $speakerName.StartsWith($_.speakerName) }).speakerUuid
-  $styles = ($speakers | Where-Object speakerUuid -eq $uuid).styles
+function Find-SpeakerByName($name, $speakers) {
+  $longestMatch = $null
+  $maxLength = 0
 
-  Write-Host ($speakerName + ":" + $uuid)
-
-  $styleId = $styles[0].styleId
-  $styles | ForEach-Object {
-    if ($speakerName.Contains($_.styleName )) {
-      $styleId = $_.styleId
+  $speakers | ForEach-Object {
+    $speakerName = $($_.speakerName)
+    if ($name.StartsWith($speakerName)) {
+      $l = $speakerName.length
+      if ($l -gt $maxLength) {
+        $longestMatch = $_
+        $maxLength = $l
+      }
     }
   }
 
-  return @{
-    uuid    = $uuid
-    styleId = $styleId
+  return $longestMatch
+}
+
+function Find-StyleByName($name, $styles) {
+  $longestMatch = $styles[0].styleId
+  $maxLength = 0
+
+  $styles | ForEach-Object {
+    $styleName = $($_.styleName)
+    if ($name.Contains($styleName)) {
+      $l = $styleName.length
+      if ($l -gt $maxLength) {
+        $longestMatch = $($_.styleId)
+        $maxLength = $l
+      }
+    }
   }
-  
+
+  return $longestMatch
 }
