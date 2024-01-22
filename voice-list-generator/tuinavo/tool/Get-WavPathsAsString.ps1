20f565bd1f6c2e30d52ef1cb044e42b31a3d75c8
so-c <so_c321@hotmail.co.jp>
2024-01-22 18:48:37 +0900

    #265 設定ファイル作成効率化用のツールを追加

---
+++ voice-list-generator/tuinavo/tool/Get-WavPathsAsString.ps1
@@ -0,0 +1,47 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$number
+)
+
+function Get-VoicePaths {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$number
+    )
+
+    $baseFolderPath = "$PSScriptRoot\..\wav\Sample_voice_$number"
+
+    $folders = @("free", "for500", "for1000")
+    $result = ""
+
+    foreach ($folder in $folders) {
+        $folderPath = "$baseFolderPath`_$folder"
+       
+        $paths = Get-WavPathAsString($folderPath)
+
+        $result += $paths -join "`r`n"
+    }
+
+    return $result
+}
+
+function Get-WavPathAsString {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$plan
+    )
+
+    $result = @()
+
+    $files = (Get-ChildItem "$plan\*.wav")
+    foreach ($f in $files) {
+        $path = ((Resolve-Path $f.FullName -Relative).Substring(6) -replace '\\', '\\')
+        $result += $path
+    }
+
+    return $result -join "`r`n"
+}
+
+Get-VoicePaths $number | Set-Clipboard
+
+
