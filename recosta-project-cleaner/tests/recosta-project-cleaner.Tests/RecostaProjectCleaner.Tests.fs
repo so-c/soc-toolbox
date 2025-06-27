ab6132f03e74a2953d0f031780f4c60ca47309ad
so-c <so_c321@hotmail.co.jp>
2025-06-27 19:20:06 +0900

    #281 失敗する受入テストを追加

--- recosta-project-cleaner/tests/recosta-project-cleaner.Tests/RecostaProjectCleaner.Tests.fs
+++ recosta-project-cleaner/tests/recosta-project-cleaner.Tests/RecostaProjectCleaner.Tests.fs
@@ -2,7 +2,97 @@ module RecostaProjectCleanerTests
 
 open System
 open Xunit
+open System.Text.Json
+open RecostaProjectCleaner
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+
+[<Fact>]
+let ``file-items から未使用 ik を除去する`` () =
+    let inputJson = """
+    {
+      "file-items": [
+        { "ik": "b1d9296544a9f4a46245638a24fb3f6d47b4d27292ea6e61f8cc5ef4f8b73fcb" },
+        { "ik": "7c31b655b76fda5d5f1270feb2d1eedd94ff89ab5b5f17ef9b5015111f972388" },
+        { "ik": "f6e64de2ac18b9b01c49ca62cc21fe7cecb7ace7127e355d7624259af8754f55" },
+        { "ik": "4fe5d1068dbdfe813ce4907dd9887d2cf753f330eafd6b2d4e467772ca81c05c" }
+      ],
+      "layers": [
+        {
+          "layer-objects": [
+            {
+              "properties": {
+                "File": {
+                  "p-value": "b1d9296544a9f4a46245638a24fb3f6d47b4d27292ea6e61f8cc5ef4f8b73fcb"
+                }
+              }
+            },
+            {
+              "properties": {
+                "File": {
+                  "p-value": "7c31b655b76fda5d5f1270feb2d1eedd94ff89ab5b5f17ef9b5015111f972388"
+                }
+              }
+            }
+          ]
+        },
+        {
+          "layer-objects": [
+            {
+              "properties": {
+                "File": {
+                  "p-value": "f6e64de2ac18b9b01c49ca62cc21fe7cecb7ace7127e355d7624259af8754f55"
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+    """
+    let expectedJson = """
+    {
+      "file-items": [
+        { "ik": "b1d9296544a9f4a46245638a24fb3f6d47b4d27292ea6e61f8cc5ef4f8b73fcb" },
+        { "ik": "7c31b655b76fda5d5f1270feb2d1eedd94ff89ab5b5f17ef9b5015111f972388" },
+        { "ik": "f6e64de2ac18b9b01c49ca62cc21fe7cecb7ace7127e355d7624259af8754f55" }
+      ],
+      "layers": [
+        {
+          "layer-objects": [
+            {
+              "properties": {
+                "File": {
+                  "p-value": "b1d9296544a9f4a46245638a24fb3f6d47b4d27292ea6e61f8cc5ef4f8b73fcb"
+                }
+              }
+            },
+            {
+              "properties": {
+                "File": {
+                  "p-value": "7c31b655b76fda5d5f1270feb2d1eedd94ff89ab5b5f17ef9b5015111f972388"
+                }
+              }
+            }
+          ]
+        },
+        {
+          "layer-objects": [
+            {
+              "properties": {
+                "File": {
+                  "p-value": "f6e64de2ac18b9b01c49ca62cc21fe7cecb7ace7127e355d7624259af8754f55"
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+    """
+    let actualJson = RecostaProjectCleaner.cleanJson inputJson
+    let normalize (json: string) =
+        JsonDocument.Parse(json).RootElement.GetRawText()
+    Assert.Equal(normalize expectedJson, normalize actualJson)
